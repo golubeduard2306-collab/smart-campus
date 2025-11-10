6f8f82bc26769a2fa1e572b349d25d604de4d6ba
ALLAIN Kolan <kolan.allain@etudiant.univ-lr.fr>
2025-11-10 11:12:28 +0100

    [feature](AjouterSaController.php) Ajout de l'implementation d'un nouvelle SA depuis la page dedié

--- stack-php-smart_campus/projet_symfony/src/Controller/SaController/AjouterSaController.php
+++ stack-php-smart_campus/projet_symfony/src/Controller/SaController/AjouterSaController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller\SaController;
 
+use App\Entity\SystemeAcquisition;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AjouterSaController extends AbstractController
+class AjouterSaController extends AbstractController
 {
-    #[Route('/ajouter-sa', name: 'app_ajouter_sa')]
-    public function index(): Response
+    #[Route('/ajouter-sa', name: 'app_ajouter_sa', methods: ['GET', 'POST'])]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('ajouter_sa/index.html.twig', [
-            'controller_name' => 'AjouterSaController',
-        ]);
+        if ($request->isMethod('POST')) {
+            $sa = new SystemeAcquisition();
+
+            // Remplir les champs obligatoires
+            $sa->setDateCreation(new \DateTime());
+            $sa->setStatut('Actif'); // ou autre valeur par défaut
+
+            $em->persist($sa);
+            $em->flush();
+
+            $this->addFlash('success', 'Un nouveau SA a été ajouté à la base de données.');
+
+            return $this->redirectToRoute('app_ajouter_sa');
+        }
+
+        return $this->render('ajouter_sa/index.html.twig');
     }
 }
