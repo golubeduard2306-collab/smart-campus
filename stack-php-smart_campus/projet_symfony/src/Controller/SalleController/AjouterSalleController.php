4950a6a6df9c2a5edc2277e4f923616d2986d31d
ALLAIN Kolan <kolan.allain@etudiant.univ-lr.fr>
2025-11-10 12:56:54 +0100

    [feature](AjouterSalleController.php) Ajout de la page HTML.twig de ajouter salle + ajout fonctionnel d'une salle dans la base de donnée

--- stack-php-smart_campus/projet_symfony/src/Controller/SalleController/AjouterSalleController.php
+++ stack-php-smart_campus/projet_symfony/src/Controller/SalleController/AjouterSalleController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller\SalleController;
 
+use App\Entity\Salle;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AjouterSalleController extends AbstractController
 {
     #[Route('/ajouter-salle', name: 'app_ajouter_salle')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('ajouter_salle/index.html.twig', [
-            'controller_name' => 'AjouterSalleController',
-        ]);
+        if ($request->isMethod('POST')) {
+            $salle = new Salle();
+            $salle->setNomSalle($request->request->get('nom_salle'));
+            $salle->setEtage((int)$request->request->get('etage'));
+            $salle->setNbFenetres((int)$request->request->get('nb_fenetres'));
+            $salle->setDateCreation(new \DateTime());
+
+            $em->persist($salle);
+            $em->flush();
+
+            $this->addFlash('success', 'La salle a été ajoutée avec succès !');
+
+            return $this->redirectToRoute('app_ajouter_salle');
+        }
+
+        return $this->render('ajouter_salle/index.html.twig');
     }
 }
