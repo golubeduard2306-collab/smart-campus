7a92f2bd908efbb54ebe0af504cebb0a7e5294ee
Noé Leteurtre <noe.leteurtre49000@gmail.com>
2025-11-13 09:33:19 +0100

    [feat](ModifierSalleController.php/modifier-salle index.html.twig) Fonctionnalité Modification des informations d'une salle et vérification que la salle n'existe pas déjà. Url de la page de modification par nom de salle

--- stack-php-smart_campus/projet_symfony/src/Controller/SalleController/ModifierSalleController.php
+++ stack-php-smart_campus/projet_symfony/src/Controller/SalleController/ModifierSalleController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller\SalleController;
 
+use App\Entity\Salle;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class ModifierSalleController extends AbstractController
 {
-    #[Route('/modifier-salle', name: 'app_modifier_salle')]
-    public function index(): Response
+    #[Route('/{nom_salle}/modifier-salle', name: 'app_modifier_salle')]
+    public function modifier(string $nom_salle, Request $request, EntityManagerInterface $em): Response
     {
+        $salle = $em->getRepository(Salle::class)->findOneBy(['nom_salle' => $nom_salle]);
+
+        if (!$salle) {
+            throw $this->createNotFoundException("Salle '$nom_salle' introuvable !");
+        }
+
+
+        if ($request->isMethod('POST')) {
+            $nouveauNom = $request->request->get('nom_salle');
+
+            $salleExistante = $em->getRepository(Salle::class)->findOneBy(['nom_salle' => $nouveauNom]);
+
+            if ($salleExistante && $salleExistante->getId() !== $salle->getId()) {
+                $this->addFlash('error', 'Ce nom de salle est déjà utilisé !');
+
+                return $this->render('modifier_salle/index.html.twig', [
+                    'salle' => $salle,
+                ]);
+            }
+
+            $salle->setNomSalle($nouveauNom);
+            $salle->setEtage($request->request->get('etage'));
+            $salle->setNbFenetres($request->request->get('nb_fenetres'));
+
+            $em->flush();
+
+            $this->addFlash('success', 'Salle modifiée avec succès !');
+
+            return $this->redirectToRoute('app_modifier_salle', [
+                'nom_salle' => $salle->getNomSalle(),
+            ]);
+        }
+
         return $this->render('modifier_salle/index.html.twig', [
-            'controller_name' => 'ModifierSalleController',
+            'salle' => $salle,
         ]);
     }
 }
