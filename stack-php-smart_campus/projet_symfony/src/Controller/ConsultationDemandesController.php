d7ff5ccb93dc4c5efae1d31c6e1b59604f77dd7b
morgan <morgan.vanvelthem@etudiant.univ-lr.fr>
2025-11-14 13:39:10 +0100

    [refactor](templates/consultation_demandes,  controller/consultation_demandes): Ajout du tableau, basique.

--- stack-php-smart_campus/projet_symfony/src/Controller/ConsultationDemandesController.php
+++ stack-php-smart_campus/projet_symfony/src/Controller/ConsultationDemandesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Salle;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ConsultationDemandesController extends AbstractController
 {
     #[Route('/consultation-demandes', name: 'app_consultation_demandes')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $salles = $em ->getRepository(Salle::class)->findAll();
+
         return $this->render('consultation_demandes/index.html.twig', [
-            'controller_name' => 'ConsultationDemandesController',
+            'salles' => $salles,
         ]);
     }
 }
